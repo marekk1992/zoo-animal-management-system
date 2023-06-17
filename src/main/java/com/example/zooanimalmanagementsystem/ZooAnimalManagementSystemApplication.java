0a89812a37c7894cf9844350d799db6fb0499f9e
marek1992 <m.komaisko@gmail.com>
2023-06-17 17:29:43 +0300

    initialize empty repository

---
+++ src/main/java/com/example/zooanimalmanagementsystem/ZooAnimalManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.zooanimalmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooAnimalManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooAnimalManagementSystemApplication.class, args);
+	}
+
+}
